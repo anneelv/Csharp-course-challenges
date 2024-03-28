a08cd85e8c91ad84fe70eaf5d319dc8101201cde
anneelv <anneelvira.lauw@gmail.com>
2024-03-28 17:13:19 +1100

    Challenge Update

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,22 +1,65 @@
-﻿// - The code declares the following variables:
-//     - Variables to determine the size of the Terminal window.
-//     - Variables to track the locations of the player and food.
-//     - Arrays `states` and `foods` to provide available player and food appearances
-//     - Variables to track the current player and food appearance
+﻿string[][] userEnteredValues = new string[][]
+{
+            new string[] { "1", "2", "3"},
+            new string[] { "1", "two", "3"},
+            new string[] { "0", "1", "2"}
+};
 
-// - The code provides the following methods:
-//     - A method to determine if the Terminal window was resized.
-//     - A method to display a random food appearance at a random location.
-//     - A method that changes the player appearance to match the food consumed.
-//     - A method that temporarily freezes the player movement.
-//     - A method that moves the player according to directional input.
-//     - A method that sets up the initial game state.
+try
+{
+    Workflow1(userEnteredValues);
+    Console.WriteLine("'Workflow1' completed successfully.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("An error occured during 'Workflow1'.");
+    Console.WriteLine(ex.Message);
+}
 
-// - The code doesn't call the methods correctly to make the game playable. The following features are missing:
-//     - Code to determine if the player has consumed the food displayed.
-//     - Code to determine if the food consumed should freeze player movement.
-//     - Code to determine if the food consumed should increase player movement.
-//     - Code to increase movement speed.
-//     - Code to redisplay the food after it's consumed by the player.
-//     - Code to terminate execution if an unsupported key is entered.
-//     - Code to terminate execution if the terminal was resized.
+static void Workflow1(string[][] userEnteredValues)
+{
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            Process1(userEntries);
+            Console.WriteLine("'Process1' completed successfully.");
+            Console.WriteLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+    }
+}
+
+static void Process1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+        if (integerFormat == true)
+        {
+            if (valueEntered != 0)
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            else
+            {
+                throw new DivideByZeroException("Invalid data. User input values must be non-zero values");
+            }
+        }
+        else
+        {
+            throw new FormatException("Invalid data. User input values mus be valid integers.");
+        }
+    }
+}
